107ba45db089decc820102ccd74b18744cf9cbfe
Attila Levente EGYEDI <attila.egyedi@gmail.com>
2016-10-11 10:29:15 -0700

    Show enabled flag

--- src/main/java/org/metadatacenter/admin/task/UserProfileListAll.java
+++ src/main/java/org/metadatacenter/admin/task/UserProfileListAll.java
@@ -27,6 +27,7 @@ public class UserProfileListAll extends AbstractKeycloakReadingTask {
         out.println("Last name : " + ur.getLastName());
         out.println("Id        : " + ur.getId());
         out.println("Email     : " + ur.getEmail());
+        out.println("Enabled   : " + ur.isEnabled());
         out.printSeparator();
       }
     }
