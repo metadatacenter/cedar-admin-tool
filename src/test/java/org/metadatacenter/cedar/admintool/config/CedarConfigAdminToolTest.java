4c3dc57b319bfdcc8545c841e7cecd3a70e64763
Attila Levente EGYEDI <attila.egyedi@gmail.com>
2017-04-17 12:28:09 -0700

    Basic config test

---
+++ src/test/java/org/metadatacenter/cedar/admintool/config/CedarConfigAdminToolTest.java
@@ -0,0 +1,53 @@
+package org.metadatacenter.cedar.admintool.config;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.metadatacenter.config.CedarConfig;
+import org.metadatacenter.config.environment.CedarEnvironmentVariable;
+import org.metadatacenter.config.environment.CedarEnvironmentVariableProvider;
+import org.metadatacenter.model.SystemComponent;
+import org.metadatacenter.util.test.TestUtil;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CedarConfigAdminToolTest {
+
+  @Before
+  public void setEnvironment() {
+    Map<String, String> env = new HashMap<>();
+
+    env.put(CedarEnvironmentVariable.CEDAR_HOME.getName(), "/home/cedar/");
+
+    env.put(CedarEnvironmentVariable.CEDAR_HOST.getName(), "metadatacenter.orgx");
+
+    env.put(CedarEnvironmentVariable.CEDAR_ADMIN_USER_PASSWORD.getName(), "adminPassword");
+    env.put(CedarEnvironmentVariable.CEDAR_ADMIN_USER_API_KEY.getName(), "1234");
+
+    env.put(CedarEnvironmentVariable.CEDAR_NEO4J_TRANSACTION_URL.getName(), "http://localhost");
+    env.put(CedarEnvironmentVariable.CEDAR_NEO4J_AUTH_STRING.getName(), "Basic base64");
+
+    env.put(CedarEnvironmentVariable.CEDAR_KEYCLOAK_CLIENT_ID.getName(), "cedar-angular-app");
+
+    env.put(CedarEnvironmentVariable.CEDAR_MONGO_USER_NAME.getName(), "cedarUser");
+    env.put(CedarEnvironmentVariable.CEDAR_MONGO_USER_PASSWORD.getName(), "password");
+
+    env.put(CedarEnvironmentVariable.CEDAR_SALT_API_KEY.getName(), "saltme");
+
+    env.put(CedarEnvironmentVariable.CEDAR_LD_USER_BASE.getName(), "https://metadatacenter.org/users/");
+
+    env.put(CedarEnvironmentVariable.CEDAR_EVERYBODY_GROUP_NAME.getName(), "Everybody");
+
+    TestUtil.setEnv(env);
+  }
+
+  @Test
+  public void testGetInstance() throws Exception {
+    SystemComponent systemComponent = SystemComponent.ADMIN_TOOL;
+    Map<String, String> environment = CedarEnvironmentVariableProvider.getFor(systemComponent);
+    CedarConfig instance = CedarConfig.getInstance(environment);
+    Assert.assertNotNull(instance);
+  }
+
+}
