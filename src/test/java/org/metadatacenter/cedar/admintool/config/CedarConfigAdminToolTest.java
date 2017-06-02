c09e99317871bb4d392c6d624cb5dce774763d26
Attila Levente EGYEDI <attila.egyedi@gmail.com>
2017-06-02 16:18:29 -0700

    Refactor to facilitate dockerization

--- src/test/java/org/metadatacenter/cedar/admintool/config/CedarConfigAdminToolTest.java
+++ src/test/java/org/metadatacenter/cedar/admintool/config/CedarConfigAdminToolTest.java
@@ -25,13 +25,15 @@ public class CedarConfigAdminToolTest {
     env.put(CedarEnvironmentVariable.CEDAR_ADMIN_USER_PASSWORD.getName(), "adminPassword");
     env.put(CedarEnvironmentVariable.CEDAR_ADMIN_USER_API_KEY.getName(), "1234");
 
-    env.put(CedarEnvironmentVariable.CEDAR_NEO4J_TRANSACTION_URL.getName(), "http://localhost");
-    env.put(CedarEnvironmentVariable.CEDAR_NEO4J_AUTH_STRING.getName(), "Basic base64");
+    env.put(CedarEnvironmentVariable.CEDAR_NEO4J_USER_NAME.getName(), "name");
+    env.put(CedarEnvironmentVariable.CEDAR_NEO4J_USER_PASSWORD.getName(), "password");
 
     env.put(CedarEnvironmentVariable.CEDAR_KEYCLOAK_CLIENT_ID.getName(), "cedar-angular-app");
 
-    env.put(CedarEnvironmentVariable.CEDAR_MONGO_USER_NAME.getName(), "cedarUser");
-    env.put(CedarEnvironmentVariable.CEDAR_MONGO_USER_PASSWORD.getName(), "password");
+    env.put(CedarEnvironmentVariable.CEDAR_MONGO_APP_USER_NAME.getName(), "cedarUser");
+    env.put(CedarEnvironmentVariable.CEDAR_MONGO_APP_USER_PASSWORD.getName(), "password");
+    env.put(CedarEnvironmentVariable.CEDAR_MONGO_HOST.getName(), "localhost");
+    env.put(CedarEnvironmentVariable.CEDAR_MONGO_PORT.getName(), "27017");
 
     env.put(CedarEnvironmentVariable.CEDAR_SALT_API_KEY.getName(), "saltme");
 
