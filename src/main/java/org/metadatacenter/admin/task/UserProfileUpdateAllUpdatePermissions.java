b73a994ad13d62f602d5cddb56561ab8ff2eb062
Attila L. Egyedi <egyedia@stanford.edu>
2019-10-02 10:57:44 -0700

    Recreate cedar roles based on superrole

--- src/main/java/org/metadatacenter/admin/task/UserProfileUpdateAllUpdatePermissions.java
+++ src/main/java/org/metadatacenter/admin/task/UserProfileUpdateAllUpdatePermissions.java
@@ -1,8 +1,12 @@
 package org.metadatacenter.admin.task;
 
 import org.keycloak.representations.idm.UserRepresentation;
+import org.metadatacenter.config.BlueprintUserProfile;
 import org.metadatacenter.server.security.CedarUserRolePermissionUtil;
+import org.metadatacenter.server.security.model.user.CedarSuperRole;
 import org.metadatacenter.server.security.model.user.CedarUser;
+import org.metadatacenter.server.security.model.user.CedarUserRole;
+import org.metadatacenter.server.security.util.CedarUserUtil;
 import org.metadatacenter.server.service.UserService;
 
 import java.util.List;
@@ -21,6 +25,7 @@ public class UserProfileUpdateAllUpdatePermissions extends AbstractKeycloakReadi
 
   @Override
   public int execute() {
+    BlueprintUserProfile blueprintUserProfile = cedarConfig.getBlueprintUserProfile();
     List<UserRepresentation> userRepresentations = listAllUsersFromKeycloak();
     if (userRepresentations == null) {
       out.println("Users not found on Keycloak");
@@ -43,6 +48,14 @@ public class UserProfileUpdateAllUpdatePermissions extends AbstractKeycloakReadi
         if (user == null && !exceptionWhileReading) {
           out.error("The user was not found for id:" + userId);
         } else {
+          user.getRoles().clear();
+          for (String realRole : ur.getRealmRoles()) {
+            CedarSuperRole superRole = CedarSuperRole.forValue(realRole);
+            if (superRole != null) {
+              List<CedarUserRole> roles = CedarUserUtil.getRolesForType(blueprintUserProfile, superRole);
+              user.getRoles().addAll(roles);
+            }
+          }
           CedarUserRolePermissionUtil.expandRolesIntoPermissions(user);
           try {
             userService.updateUser(user);
