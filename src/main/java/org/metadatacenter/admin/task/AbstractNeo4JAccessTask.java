f06b2e8c82213ad1fde6f2d18207f88d3d309253
Attila Levente EGYEDI <attila.egyedi@gmail.com>
2023-11-13 18:47:05 -0800

    Read admin user directly

--- src/main/java/org/metadatacenter/admin/task/AbstractNeo4JAccessTask.java
+++ src/main/java/org/metadatacenter/admin/task/AbstractNeo4JAccessTask.java
@@ -14,8 +14,6 @@ import org.metadatacenter.server.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.List;
-
 public abstract class AbstractNeo4JAccessTask extends AbstractKeycloakReadingTask {
 
   private static final Logger log = LoggerFactory.getLogger(AbstractNeo4JAccessTask.class);
@@ -32,14 +30,7 @@ public abstract class AbstractNeo4JAccessTask extends AbstractKeycloakReadingTas
   }
 
   protected AdminServiceSession createUnconditionalCedarAdminSession(CedarConfig cedarConfig) {
-    UserRepresentation adminRepresentation = null;
-
-    List<UserRepresentation> userRepresentations = listAllUsersFromKeycloak();
-    for (UserRepresentation ur : userRepresentations) {
-      if (cedarConfig.getAdminUserConfig().getUserName().equals(ur.getUsername())) {
-        adminRepresentation = ur;
-      }
-    }
+    UserRepresentation adminRepresentation = getAdminUserFromKeycloak();
 
     CedarUser fakeAdminExtract = new CedarUser();
     fakeAdminExtract.setFirstName(adminRepresentation.getFirstName());
