29ef0c6724b42484719e2d188c1e93524c87590f
Attila Levente EGYEDI <attila.egyedi@gmail.com>
2024-05-31 12:40:38 -0700

    Do not export incoming arcs

--- src/main/java/org/metadatacenter/admin/task/importexport/ImportExportConstants.java
+++ src/main/java/org/metadatacenter/admin/task/importexport/ImportExportConstants.java
@@ -4,6 +4,6 @@ public abstract class ImportExportConstants {
 
   public static final String CONTENT_SUFFIX = "content.json";
   public static final String NODE_SUFFIX = "resource.json";
-  public static final String OUTGOING_SUFFIX = "arcs-outgoing.json";
+  //public static final String OUTGOING_SUFFIX = "arcs-outgoing.json";
   public static final String INCOMING_SUFFIX = "arcs-incoming.json";
 }
